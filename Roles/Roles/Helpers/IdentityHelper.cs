454da9fa13af7c33dd67b097ce79b8dd701d71c6
Greg Flynn <gflynn@covdnssrv.co.volusia.fl.us>
2016-06-09 11:17:26 -0400

    Added code to add user and add user to role if they do not exist

--- Roles/Roles/Helpers/IdentityHelper.cs
+++ Roles/Roles/Helpers/IdentityHelper.cs
@@ -13,12 +13,14 @@ namespace Roles.Helpers
     {
         internal static void SeedIdEntities(DbContext context)
         {
+            //Get Managers
             var userManager = new UserManager<ApplicationUser>(
                 new UserStore<ApplicationUser>(context));
 
             var roleManager = new RoleManager<IdentityRole>(
                 new RoleStore<IdentityRole>(context));
 
+            //Create Roles if they do NOT exist
             if (!roleManager.RoleExists(RoleNames.ROLE_ADMIN))
             {
                 var roleResult = roleManager.Create(new IdentityRole(RoleNames.ROLE_ADMIN));
@@ -27,6 +29,28 @@ namespace Roles.Helpers
             if (!roleManager.RoleExists(RoleNames.ROLE_GEN))
             {
                 var roleResult = roleManager.Create(new IdentityRole(RoleNames.ROLE_GEN));
+            }
+
+            //Create Users
+            string userName = "gflynn@volusia.org";
+            string password = "Passw0rd!";
+
+            ApplicationUser user = userManager.FindByName(userName);
+            if(user == null)
+            {
+                user = new ApplicationUser()
+                {
+                    UserName = userName,
+                    Email = userName,
+                    EmailConfirmed = true
+                };
+                IdentityResult userResult = userManager.Create(user, password);
+
+                //If User is created successfully, add to Admin Role
+                if (userResult.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, RoleNames.ROLE_ADMIN);
+                }
             }
         }
     }
