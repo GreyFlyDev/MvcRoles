b95b74543270467bdf04ce39dfcf78b403e789fb
Greg Flynn <gflynn@covdnssrv.co.volusia.fl.us>
2016-06-09 11:10:21 -0400

    Added Helper Folder. Added IdentityHelper to Folder. SeedEntities Method in Identity Helper Creates Roles if Roles do not exist

---
+++ Roles/Roles/Helpers/IdentityHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Roles.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Roles.Helpers
+{
+    public class IdentityHelper
+    {
+        internal static void SeedIdEntities(DbContext context)
+        {
+            var userManager = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+
+            var roleManager = new RoleManager<IdentityRole>(
+                new RoleStore<IdentityRole>(context));
+
+            if (!roleManager.RoleExists(RoleNames.ROLE_ADMIN))
+            {
+                var roleResult = roleManager.Create(new IdentityRole(RoleNames.ROLE_ADMIN));
+            }
+
+            if (!roleManager.RoleExists(RoleNames.ROLE_GEN))
+            {
+                var roleResult = roleManager.Create(new IdentityRole(RoleNames.ROLE_GEN));
+            }
+        }
+    }
+}
